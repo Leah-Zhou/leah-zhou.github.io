0c5750b530f2beec63132f17b4bf59c7e93a1755
leahchow <ctzhouleya@gmail.com>
2020-11-16 18:09:48 -0500

    add nav

--- myportfolio/src/index.js
+++ myportfolio/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './style.css';
 import Nav from './nav';
 import reportWebVitals from './reportWebVitals';
 
 function Page() {
   return (<div><Nav />
-  <button className="btn">hola</button>
   </div> )
 }
 
--- myportfolio/src/nav.js
+++ myportfolio/src/nav.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import logo from './icons/logo.svg'
+
 
 const Nav = () => {
   return ( <div>
-    hi,no way
+        <nav>
+          <ul>
+            <li>
+            <a href="myportfolio\public\index.html"><img src ={logo} alt="logo" className="icon-logo"/></a>
+            </li>
+            <li className="menu">
+              <div> 
+                <div className="menu-square"></div>
+                <div className="menu-square"></div>
+              </div>
+              <div>
+                <div className="menu-square"></div>
+                <div className="menu-square"></div>
+              </div>
+            </li>
+          </ul>
+        </nav>
     </div>
      );
 }
