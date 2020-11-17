2f8d8cbc855f8cd8b0f0eafc0d56c5028c1bb276
leahchow <ctzhouleya@gmail.com>
2020-11-17 10:00:25 -0500

    restyle the content

--- myportfolio/src/components/content.js
+++ myportfolio/src/components/content.js
@@ -7,11 +7,19 @@ const Content = () => {
 
   return ( 
   <div>
-    <ul style={{marginTop:"5em"}}>
+    <ul className="container">
       <li>
       <img className="phone" src={Phone} alt="phone" />
       <img className="show-case" src={ShowCase} alt="green p showcase" />
+      <h3 className="project-title">GREEN P PARKING REDEIGN PROJECT</h3>
       </li>
+      <li>
+      <img className="phone" src={Phone} alt="phone" />
+      <img className="show-case" src={ShowCase} alt="green p showcase" />
+      <h3 className="project-title">GREEN P PARKING REDEIGN PROJECT</h3>
+      </li>
+    </ul>
+    <ul>
     </ul>
   </div> );
 }
